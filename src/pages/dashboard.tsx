816276f9b00f7910cba154ca2da0aa6b6c5cf190
G. Braz <gestao@vivaeventos.com.br>
2025-11-12 13:54:41 -0300

    feat: Remover prop 'usuario' do componente Header no Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -88,7 +88,7 @@ export default function DashboardPage() {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
-      <Header usuario="Administrador" />
+      <Header />
 
       {/* Conteúdo Principal */}
       <main className="container mx-auto px-4 py-8">
